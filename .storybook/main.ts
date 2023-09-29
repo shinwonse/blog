961bd4d0df7411547f2e9782d5196b688b559b07
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-09-29 13:30:21 +0900

    path alias 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
+import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -15,5 +17,17 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          extensions: config.resolve.extensions,
+        }),
+      ];
+    }
+    return config;
+  },
 };
+
 export default config;
