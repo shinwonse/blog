8661fe7320d26f1d0d0a5b9ac93740b81268590d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-10-08 22:56:57 +0900

    간격 수정

--- src/app/components/IntroductionSection/IntroductionSection.tsx
+++ src/app/components/IntroductionSection/IntroductionSection.tsx
@@ -6,9 +6,9 @@ import Section from '@/app/components/Section';
 function IntroductionSection() {
   return (
     <Section className={clsx('w-full')} layout="flex">
-      <div className={clsx('order-1 sm:-order-1')}>
-        <h2 className={clsx('text-3xl font-bold')}>신원세입니다</h2>
-        <p className={clsx('mt-2')}>평생 재미있게 개발하고 싶습니다</p>
+      <div className={clsx('order-1 mt-2 sm:-order-1 sm:mt-0')}>
+        <h2 className={clsx('text-3xl font-bold', 'mb-2')}>신원세입니다</h2>
+        <p>평생 재미있게 개발하고 싶습니다</p>
       </div>
       <Image
         alt="profile"
