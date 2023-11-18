de03db6e761931248cbea0059715d454bd1645be
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-11-18 18:51:54 +0900

    lint 일괄 적용

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -11,7 +11,7 @@ function Card({ className, description, title }: CardProps) {
     <div
       className={cn(
         'flex flex-col gap-2 rounded-xl bg-neutral-50 p-8 shadow-xl hover:scale-[1.01] dark:bg-neutral-800',
-        className
+        className,
       )}
     >
       <h3 className={cn('text-xl font-bold')}>{title}</h3>
