9e1c2e276a1d3d01664300a4da2c1ece4de78972
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-01-07 18:41:35 +0900

    그리드 수정

--- src/app/components/Section/Section.tsx
+++ src/app/components/Section/Section.tsx
@@ -15,8 +15,7 @@ function Section({ children, className, layout, title }: SectionProps) {
       {title && <h2 className={cn('mb-4 w-full font-bold')}>{title}</h2>}
       <div
         className={cn(
-          layout === 'grid' &&
-            'grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3',
+          layout === 'grid' && 'grid grid-cols-1 gap-4 md:grid-cols-3',
           layout === 'flex' &&
             'flex flex-col items-center justify-between gap-4 sm:flex-row'
         )}
