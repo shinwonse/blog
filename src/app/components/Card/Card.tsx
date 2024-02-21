ec4cec6640257a4a2ac02de88760bd48dc073d18
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-02-21 20:57:43 +0900

    포스트 카드 크기 조정

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -10,11 +10,11 @@ function Card({ className, description, title }: CardProps) {
   return (
     <div
       className={cn(
-        'flex flex-col gap-2 rounded-xl bg-neutral-50 p-6 shadow-xl hover:scale-[1.01] dark:bg-neutral-800',
+        'flex aspect-square flex-col justify-between gap-2 rounded-xl bg-neutral-50 p-6 shadow-xl hover:scale-[1.01] dark:bg-neutral-800',
         className
       )}
     >
-      <h3 className={cn('text-xl font-bold')}>{title}</h3>
+      <h3 className={cn('line-clamp-4 text-lg font-bold')}>{title}</h3>
       <p className={cn('text-sm opacity-75')}>{description}</p>
     </div>
   );
