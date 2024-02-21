e0f9cf87908278c3c1649fc1cc8af0af6af8f6ab
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-02-21 21:57:10 +0900

    카드 레이아웃 수정

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -10,12 +10,19 @@ function Card({ className, description, title }: CardProps) {
   return (
     <div
       className={cn(
-        'flex aspect-square flex-col justify-between gap-2 rounded-xl bg-neutral-50 p-6 shadow-xl hover:scale-[1.01] dark:bg-neutral-800',
+        'flex w-full flex-col justify-between gap-2 rounded-xl bg-neutral-50 p-6 shadow-xl hover:scale-[1.01] dark:bg-neutral-800',
         className
       )}
     >
-      <h3 className={cn('line-clamp-4 text-lg font-bold')}>{title}</h3>
-      <p className={cn('text-sm opacity-75')}>{description}</p>
+      <h3
+        className={cn('line-clamp-3 text-lg font-bold sm:line-clamp-2')}
+        style={{ height: '2lh' }}
+      >
+        {title}
+      </h3>
+      <p className={cn('text-sm opacity-75')} style={{ height: '2lh' }}>
+        {description}
+      </p>
     </div>
   );
 }
--- src/app/components/RecentPostSection/RecentPostSection.tsx
+++ src/app/components/RecentPostSection/RecentPostSection.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 import Card from '@/app/components/Card';
-import Section from '@/app/components/Section';
 import { getPosts } from '@/services/post';
 import { cn } from '@/utils/cn';
 
@@ -9,13 +8,14 @@ async function RecentPostSection() {
   const posts = await Promise.resolve(getPosts());
 
   return (
-    <Section className={cn('w-full')} layout="grid" title="최근 포스트">
+    <section className={cn('flex w-full flex-col gap-4')}>
+      <h2 className={cn('w-full font-bold')}>최근 포스트</h2>
       {posts.map(({ description, slug, title }) => (
         <Link key={title} href={`posts${slug}`}>
           <Card description={description} title={title} />
         </Link>
       ))}
-    </Section>
+    </section>
   );
 }
 
