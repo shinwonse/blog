aa8e16bc5c773cc7de0810acd14c7d5063d43f56
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-05-15 22:59:04 +0900

    storybook 업데이트

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,19 +4,22 @@ import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
   webpackFinal: async (config) => {
     if (config.resolve) {
       config.resolve.plugins = [
@@ -28,6 +31,10 @@ const config: StorybookConfig = {
     }
     return config;
   },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 
 export default config;
