3a1c54b515d4e28b46ed9e968e85d7f79afb9fae
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-06-04 21:38:14 +0900

    전체 포스트 리스트 모아보는 페이지 생성

--- src/app/(post)/posts/page.tsx
+++ src/app/(post)/posts/page.tsx
@@ -1,8 +1,25 @@
-const Posts = () => {
+import Link from 'next/link';
+
+import Card from '@/app/components/Card';
+import { getPosts } from '@/services/post';
+import { cn } from '@/utils/cn';
+
+const Posts = async () => {
+  const posts = await getPosts();
+
   return (
-    <div>
-      <h1>Posts</h1>
-    </div>
+    <main className={cn('flex w-full flex-col items-center px-6')}>
+      <h1 className={cn('mb-8 text-3xl font-bold')}>Posts</h1>
+      <ul className={cn('flex w-full flex-col gap-4')}>
+        {posts.map((post) => (
+          <li key={post.title}>
+            <Link href={`posts${post.slug}`}>
+              <Card description={post.description} title={post.title} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 };
 
