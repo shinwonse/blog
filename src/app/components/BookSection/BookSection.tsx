cba6558caf32c92a6653121d4988a487700e03a6
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-09-30 22:21:59 +0900

    개발 서적 읽기 섹션 UI 오류 수정

--- src/app/components/BookSection/BookSection.tsx
+++ src/app/components/BookSection/BookSection.tsx
@@ -6,9 +6,9 @@ async function BookSection() {
   const books = await Promise.resolve(getBooks());
 
   return (
-    <section className={cn('size-full px-6 content-visibility-auto')}>
+    <section className={cn('size-full h-full px-6 content-visibility-auto')}>
       <h2 className={cn('mb-4 w-full font-bold')}>개발 서적 읽기</h2>
-      <div className={cn('grid h-full grid-cols-1 gap-10 sm:gap-3')}>
+      <div className={cn('grid h-full grid-cols-1 gap-3')}>
         {books.map((book) => (
           <PostPreview
             key={book.title}
--- src/app/components/PostPreview/PostPreview.tsx
+++ src/app/components/PostPreview/PostPreview.tsx
@@ -1,4 +1,3 @@
-import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { cn } from '@/utils/cn';
 
@@ -10,10 +9,7 @@ type Props = {
 
 function PostPreview({ author, coverImage, title }: Props) {
   return (
-    <AspectRatio
-      className={cn('flex h-16 flex-row items-center gap-4 rounded')}
-      ratio={10}
-    >
+    <div className={cn('flex h-16 flex-row items-center gap-4 rounded')}>
       <Avatar className={cn('size-16 bg-white')}>
         <AvatarImage alt="Random image" src={coverImage} />
         <AvatarFallback>👋</AvatarFallback>
@@ -22,7 +18,7 @@ function PostPreview({ author, coverImage, title }: Props) {
         <h3>{title}</h3>
         <span className={cn('text-xs text-gray-400')}>{author}</span>
       </div>
-    </AspectRatio>
+    </div>
   );
 }
 
