3421cfe4e4f2ad3dfd75efaef09bc769fb48a0f7
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-12-01 20:09:35 +0900

    TOC 컴포넌트 개선 (스크롤 효과)

--- src/app/components/TableOfContents/TableOfContents.tsx
+++ src/app/components/TableOfContents/TableOfContents.tsx
@@ -1,10 +1,9 @@
-import { cn } from '@/utils/cn';
+'use client';
 
-type TOCItem = {
-  id: string;
-  tagName: string;
-  text: string;
-};
+import type { MouseEvent } from 'react';
+
+import type { TOCItem } from '@/types/notion';
+import { cn } from '@/utils/cn';
 
 type Props = {
   toc: TOCItem[];
@@ -15,12 +14,24 @@ const TableOfContents = ({ toc }: Props) => {
     return null;
   }
 
+  const handleClick = (e: MouseEvent<HTMLAnchorElement>, id: string) => {
+    e.preventDefault();
+    document.querySelector(`#${id}`)?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className={cn('border-l-2 border-stone-600 pl-4 text-sm text-stone-400')}>
       <ul>
         {toc.map(({ id, tagName, text }) => (
-          <li key={id} className={cn('w-full py-1')}>
-            <a className={cn('block truncate')} href={`#${id}`}>
+          <li
+            key={id}
+            className={cn('w-full py-1', tagName === 'h2' && 'pl-4', tagName === 'h3' && 'pl-8')}
+          >
+            <a
+              className={cn('block truncate transition-colors hover:text-stone-200')}
+              href={`#${id}`}
+              onClick={(e) => handleClick(e, id)}
+            >
               {text}
             </a>
           </li>
