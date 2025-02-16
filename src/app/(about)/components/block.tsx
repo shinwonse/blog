e47bf7e4fdf00ade12ec51a1334ea88ec4dcbb3b
shinwonse <sinwonse@gmail.com>
2025-02-16 22:08:42 +0900

    Upgrade Sharp to version 0.33.5 and minor formatting adjustments

--- src/app/(about)/components/block.tsx
+++ src/app/(about)/components/block.tsx
@@ -1,37 +1,37 @@
 import { cn } from '@/utils/cn';
 
 export type BlockContent = {
-  dateRange: string;
-  department: string;
-  description?: string;
-  englishName: string;
-  koreanName: string;
+	dateRange: string;
+	department: string;
+	description?: string;
+	englishName: string;
+	koreanName: string;
 };
 
 type BlockProps = {
-  blockContent: BlockContent;
-  className?: string;
+	blockContent: BlockContent;
+	className?: string;
 };
 
 const Block = ({ blockContent, className }: BlockProps) => {
-  const { dateRange, department, englishName, koreanName } = blockContent;
+	const { dateRange, department, englishName, koreanName } = blockContent;
 
-  return (
-    <div className={cn(className, 'flex w-full flex-row items-center')}>
-      <div className={cn('w-32 text-xs sm:w-48 sm:text-base')}>{dateRange}</div>
-      <div className={cn('flex flex-col')}>
-        <span className={cn('mr-1 text-sm font-medium sm:text-base')}>
-          {englishName}
-        </span>
-        <span className={cn('text-xs text-neutral-400 sm:text-sm')}>
-          {koreanName}
-        </span>
-        <p className={cn('flex flex-row text-xs text-neutral-400 sm:text-sm')}>
-          <span>{department}</span>
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={cn(className, 'flex w-full flex-row items-center')}>
+			<div className={cn('w-32 text-xs sm:w-48 sm:text-base')}>{dateRange}</div>
+			<div className={cn('flex flex-col')}>
+				<span className={cn('mr-1 text-sm font-medium sm:text-base')}>
+					{englishName}
+				</span>
+				<span className={cn('text-xs text-neutral-400 sm:text-sm')}>
+					{koreanName}
+				</span>
+				<p className={cn('flex flex-row text-xs text-neutral-400 sm:text-sm')}>
+					<span>{department}</span>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Block;
